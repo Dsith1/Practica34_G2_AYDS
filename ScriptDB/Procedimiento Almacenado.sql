e1cb85bde1c931e14bedf92781591d5c77a78849
Dannek Miranda <mdannek@outlook.com>
2019-10-14 23:26:59 -0600

    Procedimientos Sql transferencia y getsaldo,Tabla HIstorial

--- ScriptDB/Procedimiento Almacenado.sql
+++ ScriptDB/Procedimiento Almacenado.sql
@@ -5,3 +5,16 @@ create procedure CrearUsuario(
 as
 insert into usuario(dpi,nombres,apellidos,correo,password)
               values(@dpi_i,@nombres_i, @apellidos_i,@correo_i, @password_i);
+			  
+			  
+create procedure Tranferencia(
+	@cuenta int,  @monto int
+)
+as	
+	Update cuenta set saldo = @monto Where no_cuenta=@cuenta;
+	
+create procedure Get_Saldo(
+	@cuenta int
+)
+as 
+	Select saldo from cuenta where no_cuenta=@cuenta
